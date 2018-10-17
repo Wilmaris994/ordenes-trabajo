fe249bcff9a20cf64790491650d69b5e9ced4491
dluccajose <dluccajose@gmail.com>
2018-10-17 13:01:44 -0400

    Agregado DatePicker para los campos de fecha

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,23 @@ $(document).ready(function() {
         swal("Enviado!", "La informacion ha sido enviada al servidor!", "success");
     })
 
+
+    // Fecha para mostrar en el DatePicker
+    var fecha = new Date();
+    var fechaActual = fecha.getDate() + "/" + (fecha.getMonth() +1) + "/" + fecha.getFullYear();
+
+    $('#datepicker').datepicker({
+        uiLibrary: 'bootstrap4',
+        value: fechaActual,
+        format: 'dd/mm/yyyy'
+    });
+
+    $('#datepicker2').datepicker({
+        uiLibrary: 'bootstrap4',
+        value: fechaActual,
+        format: 'dd/mm/yyyy'
+    });
+
 });
 
 // Limpiar formulario de inicio
