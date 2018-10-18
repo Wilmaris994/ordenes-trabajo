3e96868760a23bcc12b8d593171727ca63c377e7
dluccajose <dluccajose@gmail.com>
2018-10-17 22:31:44 -0400

    Validaciones en el formulario de login

--- js/login.js
+++ js/login.js
@@ -1,15 +1,33 @@
+
+// Comprobar datos del login y validacion
 function Login() {
     var usuario = $("#username").val();
     var password = $("#password").val();
 
+    if (usuario === "" || password === "") {
 
-    if()
-    if(usuario==="admin"  && password==="admin") {
-        swal("Bienvenido Admin!", "Has iniciado sesion correctamente!", "success");
-        setTimeout(function() {
-            location.href="index.html";
-        },2000);
+        if(usuario === "") {
+            $("#user").addClass("has-error");
+        } else{
+            $("#user").removeClass("has-error");
+        }
+        if(password === "") {
+            $("#pass").addClass("has-error");
+        } else {
+            $("#pass").removeClass("has-error");
+        }
     } else {
-        swal("Datos Incorrectos!", "El correo electronico o la contraseña no son correctos, intentelo de nuevo!", "error");
+        $("#user").removeClass("has-error");
+        $("#pass").removeClass("has-error");
+        if (usuario === "admin" && password === "admin") {
+            swal("Bienvenido Admin!", "Has iniciado sesion correctamente!", "success");
+            setTimeout(function () {
+                location.href = "index.html";
+            }, 2000);
+        } else {
+            swal("Datos Incorrectos!", "El correo electronico o la contraseña no son correctos, intentelo de nuevo!", "error");
+        }
+
     }
+
 }
